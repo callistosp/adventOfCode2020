68a05e53262f932899ce885775120adf6b37624c
samc <samc@metrumrg.com>
2020-12-02 00:17:07 -0600

    day 2 solution

---
+++ day2/day2.R
@@ -0,0 +1,39 @@
+library(tidyverse)
+
+input <- data.table::fread("day2/input.txt", header=FALSE)
+input
+
+## clean and rename columns
+clean <- input %>% 
+  mutate(
+    letter=substr(V2,1,1)
+  ) %>%   
+  separate(V1, c("min.letter", "max.letter"), "-") %>% 
+  mutate_at(vars(min.letter, max.letter), as.numeric) %>% 
+  select(min.letter, max.letter, letter, password=V3)
+head(clean)
+str(clean)
+
+#### PART ONE SOLUTION
+clean %>% 
+  mutate(
+    ## count number of letters
+    n.letter = str_count(password, letter)
+  ) %>% 
+  mutate(
+    ## verify between values
+    valid = n.letter >= min.letter & n.letter <= max.letter
+  ) %>% 
+  ## count number of valid passwords
+  count(valid)
+
+#### PART TWO SOLUTION
+clean %>% 
+  mutate(
+    valid = xor(
+      substr(password, min.letter, min.letter) == letter,
+      substr(password, max.letter, max.letter) == letter
+    )
+  ) %>% 
+  ## count number of valid passwords
+  count(valid)
